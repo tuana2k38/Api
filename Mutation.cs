2e32fe370adb8d95e3433b5c2245d03b5284689a
TuanNH <Huy Tun>
2022-12-03 09:13:57 +0700

    add authen

--- Mutation.cs
+++ Mutation.cs
@@ -1,23 +1,27 @@
-public class Mutation 
+using HotChocolate.AspNetCore.Authorization;
+
+public class Mutation
 {
+    [Authorize(Policy = "tuana2k38")]
     public async Task<AuthorPayload> AddAuthor(AuthorInput input, [Service] Repository repository)
     {
-        var author = new Author(Guid.NewGuid(), input.name);
+        var author = new Author(Guid.NewGuid(), input.Name);
         await repository.AddAuthor(author);
         return new AuthorPayload(author);
     }
 
     public async Task<BookPayload> AddBook(BookInput input, [Service] Repository repository)
     {
-        var author = await repository.GetAuthor(input.author) ?? 
-                        throw new Exception("Author not found");
-        var book = new Book(Guid.NewGuid(), input.title, author);
+        var author = await repository.GetAuthor(input.Author) ??
+                        throw new AuthorNotFoundException("Author not found");
+        var book = new Book(Guid.NewGuid(), input.Title, author);
         await repository.AddBook(book);
         return new BookPayload(book);
-    }    
+    }
 }
 
-public record BookPayload(Book? record, string? error = null);
-public record BookInput(string title, Guid author);
-public record AuthorPayload(Author record);
-public record AuthorInput(string name);
+public record BookPayload(Book? Record, string? Error = null) : Payload(Error);
+public record BookInput(string Title, Guid Author);
+public record AuthorPayload(Author? Record, string? Error = null) : Payload(Error);
+public record AuthorInput(string Name);
+public record Payload(string? Error);
--- Program.cs
+++ Program.cs
@@ -2,10 +2,14 @@
 var builder = WebApplication.CreateBuilder(args);
 builder.Services
     .AddSingleton<Repository>()
+    .AddAuthentication().Services // Configure your Authentication here
+    .AddAuthorization(o => o.AddPolicy("Librarian", p => p.RequireAssertion(_ => false)))
     .AddGraphQLServer()
     .AddQueryType<Query>()
-    .AddMutationType<Mutation>();
-    
+    .AddMutationType<Mutation>()
+    .AddAuthorization()
+    .UseField<DomainExceptionMiddleware>();
+
 var app = builder.Build();
 
 app.MapGraphQL();
